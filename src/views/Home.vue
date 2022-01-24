450ba210c6bf0a402694a79836c2e0c0fe859d02
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-01-23 23:00:59 -0700

    iconify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="home">
-    <h1 class="text-9xl shadow-lg shadow-red-500/50>Family">Home</h1>
+    <h1 class="text-9xl shadow-lg shadow-red-500/50>Family">
+      Home <Icon class="icon" inline="true" width="5rem" icon="mdi-light:home" />
+    </h1>
     <!-- <weather /> -->
   </div>
 </template>
 
 <script>
 // import Weather from "../components/Weather.vue";
+import { Icon } from "@iconify/vue";
 
 export default {
   name: "Home",
   components: {
+    Icon,
     // Weather,
   },
 };
 </script>
+
+<style lang="scss">
+.icon {
+  position: absolute;
+  margin-top: -6.4rem;
+  margin-left: 55rem;
+}
+</style>
