9f00b23a19937097dc42f2aafdf3e0de6822b999
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-04-28 21:18:56 -0700

    mobile close

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,7 @@ export default {
     <button @click="isShow = !isShow" class="drop_btn">Rooms</button>
     <div v-show="isShow" class="drop_outer">
       <div class="drop_menu flex flex-col">
-        <button @click="isShow = !isShow" class="drop_close text-5xl">X</button>
+        <button @click="isShow = !isShow" class="drop_close text-6xl">X</button>
         <button class="bb1 text-7xl">Living Room</button>
         <button class="bb2 text-7xl">Family Room</button>
         <button class="bb3 text-7xl">Master Bed</button>
@@ -195,7 +195,7 @@ export default {
   height: 20px;
   z-index: 1;
   position: absolute;
-  top: 5%;
+  top: 3%;
   right: 60px;
 
   transition: all 0.3s ease-in-out;
